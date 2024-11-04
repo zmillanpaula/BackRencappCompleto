f3837b697e0ee75738603f25877d613f7b619203
FabianCanales <fabian.canales.salazar@gmail.com>
2024-11-04 17:49:28 -0300

    Agregado Logica y funcionalidad Google Cloud Storage 04/11/24

---
+++ src/main/java/generation/rencapp/api/DocumentoRestController.java
@@ -0,0 +1,77 @@
+package generation.rencapp.api;
+
+import generation.rencapp.models.Documento;
+import generation.rencapp.services.GoogleCloudStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/files")
+@CrossOrigin("*")
+public class DocumentoRestController {
+
+    @Autowired
+    private GoogleCloudStorageService storageService;
+
+    // Subir archivo para un usuario
+    @PostMapping("/upload/usuario/{usuarioId}")
+    public String uploadFileForUsuario(@PathVariable Long usuarioId, @RequestParam("file") MultipartFile file) throws IOException {
+        return storageService.uploadFileForUsuario(usuarioId, file);
+    }
+
+    // Subir archivo para una agendamiento
+    @PostMapping("/upload/agendamiento/{agendamientoId}")
+    public String uploadFileForAgendamiento(@PathVariable Long agendamientoId, @RequestParam("file") MultipartFile file) throws IOException {
+        return storageService.uploadFileForAgendamiento(agendamientoId, file);
+    }
+
+    // Listar archivos para un usuario
+    @GetMapping("/list/usuario/{usuarioId}")
+    public List<Documento> listFilesForUsuario(@PathVariable Long usuarioId) {
+        return storageService.listFilesForUsuario(usuarioId);
+    }
+
+    // Listar archivos para una agendamiento
+    @GetMapping("/list/agendamiento/{agendamientoId}")
+    public List<Documento> listFilesForAgendamiento(@PathVariable Long agendamientoId) {
+        return storageService.listFilesForAgendamiento(agendamientoId);
+    }
+
+    /*
+    // Descargar archivo
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String fileName) {
+        byte[] data = storageService.downloadFile(fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(data);
+    }*/
+  //Descarga un archivo  por un usuario
+    @GetMapping("/download/{usuarioId}/{fileName}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable Long usuarioId, @PathVariable String fileName) {
+        String fullPath = "usuario/" + usuarioId + "/documento/" + fileName;
+        System.out.println(fullPath);
+        byte[] data = storageService.downloadFile(fullPath);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(data);
+    }
+
+    // Eliminar archivo
+    @DeleteMapping("/delete/{usuarioId}/{fileName}")
+    public String deleteFile(@PathVariable String fileName, @PathVariable Long usuarioId) {
+
+        String fullPath = "usuario/" + usuarioId + "/documento/" + fileName;
+        return storageService.deleteFile(fullPath);
+    }
+}
+
---
+++ src/main/java/generation/rencapp/services/GoogleCloudStorageService.java
@@ -0,0 +1,117 @@
+package generation.rencapp.services;
+
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.*;
+import generation.rencapp.models.Agendamiento;
+import generation.rencapp.models.Documento;
+import generation.rencapp.models.Usuario;
+import generation.rencapp.repositories.AgendamientoRepository;
+import generation.rencapp.repositories.DocumentoRepository;
+import generation.rencapp.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class GoogleCloudStorageService {
+    // @Value("${google.cloud.storage.bucket-name}")
+    private String bucketName = "example_bucket_renca";
+    private final String SERVICE_ACCOUNT_JSON_PATH = "C:\\Users\\Lenovo\\Downloads\\careful-muse-438313-k1-10a632007159.json";
+    private final Storage storage;
+    // private final String GOOGLE_APPLICATION_CREDENTIALS = "C:\\Users\\p\\Desktop\\prueba\\analog-oven-438313-i2-b3b195ffa242.json";
+
+
+    @Autowired
+    private DocumentoRepository documentoRepository;
+
+    @Autowired
+    private AgendamientoRepository agendamientoRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    {
+        try {
+            // System.out.println("Aca empieza el codigo");
+            storage = StorageOptions.newBuilder()
+                    .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(SERVICE_ACCOUNT_JSON_PATH)))
+                    .build().getService();
+            // System.out.println("Aca termina :) ");
+            // System.out.println(bucketName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Método para subir un archivo asociado a un usuario
+    public String uploadFileForUsuario(Long usuarioId, MultipartFile file) throws IOException {
+
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        String filePath = "usuarios/" + usuarioId + "/documentos/" + file.getOriginalFilename();
+
+        BlobId blobId = BlobId.of(bucketName, filePath);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(file.getContentType()).build();
+        storage.create(blobInfo, file.getBytes());
+
+        // Guardar información del archivo en la base de datos
+        Documento documento = new Documento();
+        documento.setNombreArchivo(file.getOriginalFilename());
+        documento.setUrlArchivo(filePath);
+        documento.setUsuario(usuario);
+        documentoRepository.save(documento);
+
+
+        return String.format("File %s uploaded to bucket %s as %s", file.getOriginalFilename(), bucketName, blobId.getName());
+    }
+
+    // Método para subir un archivo asociado a una agendamiento
+    public String uploadFileForAgendamiento(Long agendamientoId, MultipartFile file) throws IOException {
+        Agendamiento agendamiento = agendamientoRepository.findById(agendamientoId)
+                .orElseThrow(() -> new RuntimeException("agendamiento no encontrada"));
+
+        String filePath = "agendamientos/" + agendamientoId + "/documentos/" + file.getOriginalFilename();
+        BlobId blobId = BlobId.of(bucketName, filePath);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(file.getContentType()).build();
+        storage.create(blobInfo, file.getBytes());
+
+        // Guardar información del archivo en la base de datos
+        Documento documento = new Documento();
+        documento.setNombreArchivo(file.getOriginalFilename());
+        documento.setUrlArchivo(filePath);
+        documento.setAgendamiento(agendamiento);
+        documentoRepository.save(documento);
+
+        System.out.println(blobId);
+
+        return String.format("File %s uploaded to bucket %s as %s", file.getOriginalFilename(), bucketName, blobId.getName());
+    }
+
+    // Método para descargar un archivo
+    public byte[] downloadFile(String fileName) {
+        Blob blob = storage.get(BlobId.of(bucketName, fileName));
+        System.out.println("Aqui llega el cod" + blob.exists());
+        return blob.getContent();
+    }
+
+    // Método para listar archivos de un paciente
+    public List<Documento> listFilesForUsuario(Long usuarioId) {
+        return documentoRepository.findByUsuarioId(usuarioId);
+    }
+
+    // Método para listar archivos de una agendamiento
+    public List<Documento> listFilesForAgendamiento(Long agendamientoId) {
+        return documentoRepository.findByAgendamientoId(agendamientoId);
+    }
+
+    // Método para eliminar un archivo
+    public String deleteFile(String fileName) {
+        BlobId blobId = BlobId.of(bucketName, fileName);
+        boolean deleted = storage.delete(blobId);
+        return deleted ? "File deleted successfully" : "File not found";
+    }
+}
