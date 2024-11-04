6a1f4bfa7f359ac0a0f14f7826e7fa9be4eb8f9b
MarceloGallardo01 <marcelogallardo999@gmail.com>
2024-11-04 15:28:40 -0300

    UpDate: Documentado departamento y servicio, fecha: 04/11/24

--- src/main/java/generation/rencapp/api/DepartamentoRestController.java
+++ src/main/java/generation/rencapp/api/DepartamentoRestController.java
@@ -46,7 +46,7 @@ public class DepartamentoRestController {
     }
 }*/
 
-package generation.rencapp.API;
+package generation.rencapp.api;
 
 import generation.rencapp.models.Departamento;
 import generation.rencapp.service.DepartamentoService;
@@ -54,7 +54,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 
 @RestController
@@ -65,12 +69,26 @@ public class DepartamentoRestController {
     @Autowired
     private DepartamentoService departamentoService;
 
+
+
+    @Operation (summary = "Mostrar lista de Departamentos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de Departamentos", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Departamento.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la búsqueda de lista de departamento")
+    })
     @GetMapping("/lista")
     public ResponseEntity<List<Departamento>> buscarLista() {
         List<Departamento> departamentos = departamentoService.findAll();
         return new ResponseEntity<>(departamentos, HttpStatus.OK);
     }
 
+
+
+    @Operation (summary = "Buscar departamento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Departamento encontrado con éxito", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Departamento.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la búsqueda del departamento")
+    })
     @GetMapping
     public ResponseEntity<Departamento> buscarPorId(@RequestParam Long search) {
         Departamento departamento = departamentoService.findById(search);
@@ -81,18 +99,39 @@ public class DepartamentoRestController {
         }
     }
 
+
+
+    @Operation (summary = "Crear departamento como administrador")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Creación de departamento exitoso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Departamento.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la creación del departamento")
+    })
     @PostMapping("/crear")
     public ResponseEntity<Departamento> crearDepartamento(@RequestBody Departamento departamento) {
         Departamento nuevoDepartamento = departamentoService.save(departamento);
         return new ResponseEntity<>(nuevoDepartamento, HttpStatus.CREATED);
     }
 
+
+
+    @Operation (summary = "Eliminar departamento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Eliminación del departamento exitoso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Departamento.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la eliminación del departamento")
+    })
     @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<String> eliminarDepartamento(@PathVariable Long id) {
         departamentoService.deleteById(id);
         return new ResponseEntity<>("Departamento eliminado", HttpStatus.OK);
     }
 
+
+
+    @Operation (summary = "Editar departamento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Edición del departamento exitoso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Departamento.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la edición del departamento")
+    })
     @PutMapping("/editar/{id}")
     public ResponseEntity<Departamento> editarDepartamento(@PathVariable Long id, @RequestBody Departamento departamento) {
         departamento.setId(id);
--- src/main/java/generation/rencapp/api/ServicioRestController.java
+++ src/main/java/generation/rencapp/api/ServicioRestController.java
@@ -37,13 +37,17 @@ public class ServicioRestController {
     }
 }*/
 
-package generation.rencapp.API;
+package generation.rencapp.api;
 
 import generation.rencapp.models.Departamento;
 import generation.rencapp.models.Servicio;
 import generation.rencapp.service.DepartamentoServiceImpl;
-import generation.rencapp.service.ServicioService;
 import generation.rencapp.service.ServicioServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -62,11 +66,25 @@ public class ServicioRestController {
     @Autowired
     private DepartamentoServiceImpl departamentoService;
 
+
+
+    @Operation(summary = "Mostrar lista de Servicios")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de Servicios", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Servicio.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la creación del servicio")
+    })
     @GetMapping("/lista")
     public ResponseEntity<List<Servicio>> buscarLista() {
         return new ResponseEntity<>(servicioService.findAll(), HttpStatus.OK);
     }
 
+
+
+    @Operation(summary = "Buscar servicio por Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de Servicios", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Servicio.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la búsqueda de servicio")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Servicio> buscarPorId(@PathVariable Long id) {
         Servicio servicio = servicioService.findById(id);
@@ -77,13 +95,13 @@ public class ServicioRestController {
         }
     }
 
-    /*@PostMapping("/crear")
-    public ResponseEntity<Servicio> crearServicio(@RequestBody Servicio servicio) {
-        Servicio nuevoServicio = servicioService.save(servicio);
-        return new ResponseEntity<>(nuevoServicio, HttpStatus.CREATED);
-    }*/
 
 
+    @Operation(summary = "Ver lista de servicios por departamento Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de Servicios por departamento Id", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Servicio.class))),
+            @ApiResponse(responseCode = "401", description = "Error en búsqueda de servicios por departamento Id")
+    })
     @GetMapping("/{idDepartamento}/verlistaservicios")
     public ResponseEntity<List<Servicio>> buscarListaServiciosPorDepartamento(@PathVariable Long idDepartamento) {
         Departamento departamento = departamentoService.findById(idDepartamento);
@@ -93,6 +111,14 @@ public class ServicioRestController {
         List<Servicio> listaServicio = servicioService.findByDepartamentoId(idDepartamento);
         return new ResponseEntity<>(listaServicio, HttpStatus.OK);
     }
+
+
+
+    @Operation(summary = "Crear servicio por departamento Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Crear Servicio por departamento Id", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Servicio.class))),
+            @ApiResponse(responseCode = "401", description = "Error en la creación del servicio por departamento Id")
+    })
     @PostMapping("/{idDepartamento}/crear")
     public ResponseEntity<Servicio> crearServicio(@RequestBody Servicio servicio, @PathVariable Long idDepartamento) {
         Departamento departamento = departamentoService.findById(idDepartamento);
@@ -104,12 +130,26 @@ public class ServicioRestController {
         return new ResponseEntity<>(nuevoServicio, HttpStatus.CREATED);
     }
 
+
+
+    @Operation(summary = "Eliminar servicio")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Eliminar Servicio", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Servicio.class))),
+            @ApiResponse(responseCode = "401", description = "Error al eliminar el servicio")
+    })
     @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<String> eliminarServicio(@PathVariable Long id) {
         servicioService.deleteById(id);
         return new ResponseEntity<>("Servicio eliminado", HttpStatus.OK);
     }
 
+
+
+    @Operation(summary = "Modificar servicio")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Servicio modificado con éxito", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Servicio.class))),
+            @ApiResponse(responseCode = "401", description = "Error al modificar el servicio")
+    })
     @PutMapping("/editar/{id}")
     public ResponseEntity<Servicio> editarServicio(@PathVariable Long id, @RequestBody Servicio servicio) {
         Departamento departamento= departamentoService.findById(servicioService.findById(id).getDepartamento().getId());
